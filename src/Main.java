0b1aeeb6bf0e605a3ebd0ca4eca5e2a6a98ba971
yjs <blackgriffon@naver.com>
2019-10-31 20:13:25 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,95 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        // 1. 정답을 생성한다.
+        Random random = new Random(0);
 
-        //1
-        Random random = new Random();
+        int answer0;
+        int answer1;
+        int answer2;
 
-        //2 두번째
+        while (true) {
+            answer0 = random.nextInt(Constant.MAX_NUMBER);
+            answer1 = random.nextInt(Constant.MAX_NUMBER);
+            answer2 = random.nextInt(Constant.MAX_NUMBER);
 
-        //2
+            if (answer0 == answer1 || answer1 == answer2 || answer2 == answer0)
+                continue;
 
-        //3
+            break;
+        }
 
+        // live template
+        System.out.println("[정답]");
+        System.out.print(answer0 + " ");
+        System.out.print(answer1 + " ");
+        System.out.print(answer2 + " ");
+        System.out.println();
+
+
+        int tryCount = 0;
+
+        while (true) {
+            tryCount++;
+
+            // 2. 추측을 입력받는다.
+            int guess0;
+            int guess1;
+            int guess2;
+
+            Scanner scanner = new Scanner(System.in);
+            guess0 = scanner.nextInt(); // parsing
+            guess1 = scanner.nextInt(); // parsing
+            guess2 = scanner.nextInt(); // parsing
+
+            System.out.println("[추측]");
+            System.out.print(guess0 + " ");
+            System.out.print(guess1 + " ");
+            System.out.print(guess2 + " ");
+            System.out.println();
+
+
+            // 3. 결과를 계산한다.
+            int strike = 0;
+            int ball = 0;
+            int out = 0;
+
+            if (answer0 == guess0)
+                strike++;
+            else if (answer0 == guess1 || answer0 == guess2)
+                ball++;
+            else
+                out++;
+
+            if (answer1 == guess1)
+                strike++;
+            else if (answer1 == guess2 || answer1 == guess0)
+                ball++;
+            else
+                out++;
+
+            if (answer2 == guess2)
+                strike++;
+            else if (answer2 == guess0 || answer2 == guess1)
+                ball++;
+            else
+                out++;
+
+
+            // 4. 결과를 출력한다.
+            System.out.println(String.format("S:%d B:%d O:%d", strike, ball, out));
+
+
+            // 5. 3S 아니면 2번으로 돌아간다.
+            if (strike == Constant.DIGIT)
+                break;
+        }
+
+
+        // 6. 도전횟수를 출력한다.
+        System.out.println(String.format("Count : %d", tryCount));
     }
 }
