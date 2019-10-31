0f535c630ae3d0df12a3832321ba7562b78b182f
yjs <blackgriffon@naver.com>
2019-10-31 20:02:32 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-import java.util.Random;
-
 public class Main {
-
     public static void main(String[] args) {
+        // 1. 정답을 생성한다.
 
-        //1
-        Random random = new Random();
 
-        //2 두번째
+        // 2. 추측을 입력받는다.
 
-        //2
 
-        //3
+        // 3. 결과를 계산한다.
 
+
+        // 4. 결과를 출력한다.
+
+
+        // 5. 3S 아니면 2번으로 돌아간다.
+
+
+        // 6. 도전횟수를 출력한다.
     }
 }
